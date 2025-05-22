968203ccdbe0c592c3432c724e81f2e7fa9a3a1e
HyeranKeum <sarah2316@g.skku.edu>
2025-05-22 12:37:10 +0900

    chore(structure) : global/ 디렉토리 만듦. timeperiod 변수 dc -> global로 옮김

---
+++ src/global/global.c
@@ -0,0 +1,3 @@
+#include "hal_data.h"
+
+uint32_t Timer_Period = 0x249F00; // 20[ms] Duty Cycle (50[Hz])
---
+++ src/global/global.h
@@ -0,0 +1,8 @@
+#ifndef _FinPj_global_H_
+#define _FinPj_global_H_
+
+#include "hal_data.h"
+
+extern uint32_t Timer_Period;
+
+#endif 
